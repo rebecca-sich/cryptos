b5192a4af9ca6b0eceef011279da20da9a258e60
Sichel <rebeccas@vimeo.com>
2017-12-26 17:49:01 -0500

    Add coinbase command to Price Bot

--- pricebot.py
+++ pricebot.py
@@ -1,12 +1,14 @@
 import os
 import time
 import re
+import gdax
 from slackclient import SlackClient
 import urllib2
 from bs4 import BeautifulSoup
 
 ## Slackbot to get the price of certain cryptos from www.coinmarketcap.com
 ## With help from: https://www.fullstackpython.com/blog/build-first-slack-bot-python.html
+## GDAX Puthon API: https://github.com/danpaquin/gdax-python
 
 # instantiate Slack client
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
@@ -51,12 +53,20 @@ def handle_command(command, channel):
     response = None
     # This is where you start to implement more commands!
     if command.startswith(EXAMPLE_COMMAND):
-        response = "Type 'price' followed by the name of the crypto and I will give you the price of that currency. For example: price ripple"
+        response = "Type 'price' followed by the name of the crypto and I will give you the price of that currency. For example: price ripple \n"
+        response += "Or, type 'coinbase' and I will give you the current prices on Coinbase"
     if command.startswith("price"):
     	try:
     		response = get_price(command.split(" ")[1])
     	except: 
     		response = "What currency do you want to see? I know a bunch!"
+    if command.startswith("coinbase"):
+    	slack_client.api_call(
+        	"chat.postMessage",
+        	channel=channel,
+        	text="Checking coinbase prices, hold on!"
+    	)
+    	response = coinbase()
 
     # Sends the response back to the channel
     slack_client.api_call(
@@ -65,6 +75,7 @@ def handle_command(command, channel):
         text=response or default_response
     )
 
+## Get the price for a given coin from coinmarketcap.com
 def get_price(coin):
 	isCoin = False
 	response = None 
@@ -99,7 +110,35 @@ def get_price(coin):
 
 		price = (price_box.a).get('data-usd')
 		response = coin + " is currently $" + price 
+	
 	return response or default_response
+
+## Get the current prices from Coinbase
+def coinbase():
+	btc = "Bitcoin is: $"
+	bch = "Bitcoin Cash is: $"
+	eth = "Ether is: $"
+	ltc = "Litecoin is: $"
+
+	public_client = gdax.PublicClient()
+	BTC = public_client.get_product_ticker(product_id='BTC-USD')
+	for key in BTC:
+		if str(key) == "price":
+			btc += str(BTC[key]) + "\n"
+	BCH = public_client.get_product_ticker(product_id='BCH-USD')
+	for key in BCH:
+		if str(key) == "price":
+			bch += str(BCH[key]) + "\n"
+	ETH = public_client.get_product_ticker(product_id='ETH-USD')
+	for key in ETH:
+		if str(key) == "price":
+			eth += str(ETH[key]) + "\n"
+	LTC = public_client.get_product_ticker(product_id='LTC-USD')
+	for key in LTC:
+		if str(key) == "price":
+			ltc += str(LTC[key]) + "\n"
+	return btc + bch + eth + ltc
+
 
 if __name__ == "__main__":
     if slack_client.rtm_connect(with_team_state=False):
